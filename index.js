7df469e36d2aaff1d461e501533331a738a5cc94
Naveen Bhatia <nbhatia76@gmail.com>
2018-09-02 16:23:17 +0530

    first cut

---
+++ index.js
@@ -0,0 +1,20 @@
+const server = require('./lib/server')
+const config = require('./lib/config.js')
+
+
+if (['http', 'https'].indexOf(config.defaultServer) == -1) {
+  console.log('ERROR: Service init failed - invalid config');
+  return -1;
+}
+
+var port = (config.defaultServer == 'http') ?
+            config.httpPort :
+            config.httpsPort;
+
+var serverOptions = {
+   'keyPath': config.https.keyPath,
+   'certPath': config.https.certPath
+};
+
+console.log('Starting the App..');
+server.init(config.defaultServer, serverOptions, port);
